ac13f6cc772e98e15dda11595ebb9fe7e22463aa
Ubuntu <ubuntu@ip-172-31-34-193.us-west-2.compute.internal>
2013-07-12 23:14:06 +0000

    HW3 part1

--- web.js
+++ web.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var fs = require ('fs');
 
 var app = express.createServer(express.logger());
 
+try{
+    var data = fs.readFileSync('index.html','utf8');
+    console.log(data);
+}catch(e){
+    console.log('Error reading file index.html!');
+}
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(data);
 });
 
 var port = process.env.PORT || 5000;
